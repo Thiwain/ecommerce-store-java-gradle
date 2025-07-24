316da10b2d7ef4531a02048d588cb5164eb9e1c8
Thiwain Medagama <thiwainm@gmail.com>
2025-07-24 22:55:47 +0530

    admin-user-has-access entity

---
+++ backend/src/java/com/plato/models/AdminUserHasAccess.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.plato.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "admin_user_has_access")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AdminUserHasAccess {
+
+    @Id
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_user_id", nullable = false)
+    private AdminUser adminUser;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "access_id", nullable = false)
+    private Access access;
+
+}
